328e80938833a7ba8ca55dbdfa6b10bc9849a38c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-06-25 08:01:03 -0300

    feat(frontend): add two decimal places on save and edit

--- atv-4/backend/data/dados.js
+++ atv-4/backend/data/dados.js
@@ -3,19 +3,19 @@ const dados = [
     id: 1,
     nome: "Ana Souza",
     curso: "Engenharia da Computação",
-    IRA: 8.7
+    IRA: 8.77
   },
   {
     id: 2,
     nome: "Bruno Lima",
     curso: "Sistemas de Informação",
-    IRA: 7.9
+    IRA: 7.91
   },
   {
     id: 3,
     nome: "Camila Ribeiro",
     curso: "Ciência da Computação",
-    IRA: 9.1
+    IRA: 9.13
   }
 ];
 
